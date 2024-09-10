896e4bde8fdac73888a91a24915f4bbc02914da6
L_akheel <166927634+akheel-9495@users.noreply.github.com>
2024-09-10 12:22:44 +0530

    Add files via upload

---
+++ Spectro.py
@@ -0,0 +1,18 @@
+import librosa
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load an audio file
+y, sr = librosa.load('secretaudio_1559007588454.wav')
+
+# Generate a spectrogram
+S = librosa.feature.melspectrogram(y=y, sr=sr)
+S_dB = librosa.power_to_db(S, ref=np.max)
+
+# Display the spectrogram
+plt.figure(figsize=(10, 4))
+librosa.display.specshow(S_dB, sr=sr, x_axis='time', y_axis='mel')
+plt.colorbar(format='%+2.0f dB')
+plt.title('Mel-frequency spectrogram')
+plt.tight_layout()
+plt.show()
